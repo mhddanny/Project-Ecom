e8919783f3b6df1ca1509ec90cefc03330bb9d73
mhddanny <mhddanny1gmail.com>
2023-02-23 14:41:32 +0700

    addede allowed host

--- src/projectCart/projectCart/settings.py
+++ src/projectCart/projectCart/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool) #True
 
 ALLOWED_HOSTS = [
+    'ec2-35-166-148-8.us-west-2.compute.amazonaws.com',
     '*'
 ]
 
