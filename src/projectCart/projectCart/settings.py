3d8d31e3e633de15a9d239db4112a35529c8bdad
mhddanny <mhddanny1gmail.com>
2023-03-01 22:24:30 +0700

    update setting file

--- src/projectCart/projectCart/settings.py
+++ src/projectCart/projectCart/settings.py
@@ -139,14 +139,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 STATICFILES_DIRS = [
-    'projectCart/static',
+    os.path.join('projectCart/static'),
 ]
 
 # media File Configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
