8d5cce2d47d023f7724f07de651bec1a535f1721
mhddanny <mhddanny1gmail.com>
2023-03-22 06:23:26 +0700

    updata ip public

--- src/projectCart/projectCart/settings.py
+++ src/projectCart/projectCart/settings.py
@@ -29,7 +29,7 @@ DEBUG = config('DEBUG', default=True, cast=bool) #True
 
 ALLOWED_HOSTS = [
     'ec2-35-166-148-8.us-west-2.compute.amazonaws.com',
-    '35.166.148.8',
+    '35.163.66.179',
     '*'
 ]
 
