c5b230148c0a5217d7bda746eaed4ce3b98b4503
mhddanny <mhddanny1gmail.com>
2023-03-24 23:10:15 +0700

    update s3 amazon

--- src/projectCart/projectCart/settings.py
+++ src/projectCart/projectCart/settings.py
@@ -163,11 +163,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR / 'static')
-STATICFILES_DIRS = [
-    os.path.join('projectCart/static'),
-]
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+# STATICFILES_DIRS = [
+#     os.path.join('projectCart/static'),
+# ]
 
 
 # AWS S3 Static Files Configuration
