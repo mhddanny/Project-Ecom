a479bd618f1857d99ec043082d750c05730f9042
mhddanny <mhddanny1@gmail.com>
2023-12-13 11:26:45 +0700

    upload video category and validator

--- src/projectCart/category/models.py
+++ src/projectCart/category/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.core.validators import FileExtensionValidator
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(max_length=255, blank=True)
-    cat_image = models.ImageField(upload_to='photos/category', blank=True)
+    cat_image = models.ImageField(upload_to='photos/category', blank=True, validators=[FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg'])])
+    video_file = models.FileField(upload_to='videos/category', blank=True, validators=[FileExtensionValidator(allowed_extensions=['mp4'])])
 
     class Meta:
         verbose_name = 'category'
